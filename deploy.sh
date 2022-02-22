4e4077454e0354286155ec4afb91155dd1146ded
SRayJay <shenrayjay624@gmail.com>
2022-02-22 12:59:58 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,3 @@
-#!/usr/bin/env sh
 
 # 确保脚本抛出遇到的错误
 set -e
@@ -10,14 +9,14 @@ npm run build
 cd docs/.vuepress/dist
 
 # 如果是发布到自定义域名
-echo 'www.notenough.top' > CNAME
+# echo 'www.notenough.top' > CNAME
 
-git init
+# git init
 git add -A
 git commit -m 'deploy'
 
 # 如果你想要部署到 https://USERNAME.github.io
-git push -f git@github.com:SRayJay/SRayJay.github.io.git master
+git push -f git@github.com:SRayJay/SRayJay.github.io.git main
 
 # 如果发布到 https://USERNAME.github.io/<REPO>  REPO=github上的项目
 # git push -f git@github.com:USERNAME/<REPO>.git master:gh-pages
